f7cc671204aa6ed44e61dc982368a54fbc64620f
RareJackalope <rarejackalope@gmail.com>
2017-03-24 20:01:47 -0500

    initial commit, basic fx class that extends application and fxml from scene builder templating.

---
+++ Projects/Mini/rarejackalope/gui/textEditor/TextEditor.java
@@ -0,0 +1,15 @@
+package rarejackalope.gui.textEditor;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TextEditor extends Application
+{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
