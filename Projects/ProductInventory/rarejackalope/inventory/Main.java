84c44d835aa53de7b529a2294c733f9f692cbab8
RareJackalope <rarejackalope@gmail.com>
2017-03-27 20:28:19 -0500

    Basic setup for the rootLayout and primary stage. Next Step is to build up the view and model for products and inventory.

--- Projects/ProductInventory/rarejackalope/inventory/Main.java
+++ Projects/ProductInventory/rarejackalope/inventory/Main.java
@@ -1,6 +1,11 @@
 package rarejackalope.inventory;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 /*
  *Brief
@@ -11,15 +16,41 @@ import javafx.stage.Stage;
  */
 public class Main extends Application
 {
+	
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
 	public static void main(String[] args) 
 	{
-		
+		launch(args);
 	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception 
 	{
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Inventory Management Demo");
 		
+		initialiseRootLayout();
+	}
+	
+	
+	private void initialiseRootLayout() 
+	{
+		try
+		{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane)loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		catch(IOException exc)
+		{
+			exc.printStackTrace();
+		}
 	}
 
 }
