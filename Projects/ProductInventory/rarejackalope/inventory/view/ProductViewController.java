725a9d52bf5036cb149b7c8328fec6080413d568
RareJackalope <rarejackalope@gmail.com>
2017-03-27 22:05:55 -0500

    Finished up binding between controller and view, will need an inventory controller to properly handle summing up the total value of the product objects.

--- Projects/ProductInventory/rarejackalope/inventory/view/ProductViewController.java
+++ Projects/ProductInventory/rarejackalope/inventory/view/ProductViewController.java
@@ -1,5 +1,8 @@
 package rarejackalope.inventory.view;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -13,16 +16,55 @@ public class ProductViewController
 	@FXML private TableColumn<Product, String> productNameColumn;
 	@FXML private TableColumn<Product, Boolean> productAvaliabilityColumn;
 	@FXML private Label productNameLabel;
-	@FXML private Label pricePerUnit;
-	@FXML private Label productId;
-	@FXML private Label unitsAvaliable;
-	@FXML private Label currentStockValue;
+	@FXML private Label pricePerUnitLabel;
+	@FXML private Label productIdLabel;
+	@FXML private Label unitsAvaliableLabel;
+	@FXML private Label currentStockValueLabel;
 	
 	private Main main;
 	
 	@FXML
 	public void initialize()
 	{
+		productNameColumn.setCellValueFactory(cellData -> cellData.getValue().productName());
+		productAvaliabilityColumn.setCellValueFactory(cellData -> cellData.getValue().productInStock().asObject());
 		
+		showProductDetails(null);
+		
+		productTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showProductDetails(newValue));
+		
+	}
+
+	private void showProductDetails(Product product) 
+	{
+		if(product != null)
+		{
+			productNameLabel.setText(product.getProductName());
+			pricePerUnitLabel.setText(Double.toString(product.getPricePerProduct()));
+			productIdLabel.setText(Integer.toString(product.getProductId()));
+			unitsAvaliableLabel.setText(Integer.toString(product.getProductStockAmount()));
+			currentStockValueLabel.setText(totalProductValue(product.getPricePerProduct(), product.getProductStockAmount()));
+		}
+		else
+		{
+			productNameLabel.setText("");
+			pricePerUnitLabel.setText("");
+			productIdLabel.setText("");
+			unitsAvaliableLabel.setText("");
+			unitsAvaliableLabel.setText("");
+			currentStockValueLabel.setText("");
+		}
+	}
+
+	private String totalProductValue(double pricePerProduct, int productStockAmount) 
+	{	
+		NumberFormat formatter = new DecimalFormat("#0.00");
+		return formatter.format(pricePerProduct * productStockAmount);
+	}
+	
+	public void setMain(Main main)
+	{
+		this.main = main;
+		productTable.setItems(main.getProductData());
 	}
 }
