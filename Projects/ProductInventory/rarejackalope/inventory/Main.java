a938a06732ac955a8637521c1c262d5ef6924aaf
RareJackalope <rarejackalope@gmail.com>
2017-03-27 20:15:11 -0500

    Initial Commit of project packages following a MVC pattern.

---
+++ Projects/ProductInventory/rarejackalope/inventory/Main.java
@@ -0,0 +1,25 @@
+package rarejackalope.inventory;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+/*
+ *Brief
+ *Product Inventory Project - Create an application which manages an inventory of products. 
+ *Create a product class which has a price, id, and quantity on hand. 
+ *Then create an inventory class which keeps track of various products and can sum up the inventory value. 
+ * 
+ */
+public class Main extends Application
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception 
+	{
+		
+	}
+
+}
